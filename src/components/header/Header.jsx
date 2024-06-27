ee453dce591a22a338ec5058576cb338c7f19f03
Kaustubh Patil <patilkaustubh990@gmail.com>
2024-06-27 21:52:39 +0530

    chore: Update fonts and header component styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,17 +2,30 @@ import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import { twMerge } from 'tailwind-merge'
+import { merge } from '@/lib/utils'
 
 const Header = () => {
   return (
     <div>
-        <div>
-            <Link href="/">
-            <div>
-                <h2 className='pl-10 pt-2'>KanBAN</h2>
-            </div>
-            </Link>
-        </div>
+      <div className='hidden md:flex'>
+        <Link href="/">
+          <div className='hover:opacity-45 transition flex items-center gap-x-2 '>
+            <Image src="/logo.png" alt="logo" width={60} height={30} className='' />
+            {/* <h2 className={merge("text-lg text-slate-700 pb-1 ")}>KanBAN</h2> */}
+            <h2 className={merge("text-lg text-slate-700 pb-1 custom-font")}>KanBAN</h2>
+          </div>
+        </Link>
+      </div>
+
+      {/* clerk auth */}
+      <div>
+        <Link href="/auth/signin">
+          <span className={twMerge("text-slate-700 hover:text-slate-900")}>Sign in</span>
+        </Link>
+        <Link href="/auth/signup">
+          <span className={twMerge("text-slate-700 hover:text-slate-900")}>Sign up</span>
+        </Link>
+      </div>
     </div>
   )
 }
