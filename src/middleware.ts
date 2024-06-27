a17a6cd9b02af1130f535e0c6a4f8af32619dec9
Kaustubh Patil <patilkaustubh990@gmail.com>
2024-06-28 03:09:44 +0530

    chore: Update auth middleware to redirect to organization selection page

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,12 +5,15 @@ export default authMiddleware({
   afterAuth(auth, req, evt) {
     // Handle users who aren't authenticated
     if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
+      let path = '/';
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+      // return redirectToSignIn({ returnBackUrl: req.url });
     }
     // Redirect signed in users to organization selection page if they are not active in an organization
     if (
-      auth.userId &&
-      !auth.orgId &&
+      !auth.userId &&
+      !auth.isPublicRoute &&
       req.nextUrl.pathname !== "/org-selection"
     ) {
       const orgSelection = new URL("/org-selection", req.url);
