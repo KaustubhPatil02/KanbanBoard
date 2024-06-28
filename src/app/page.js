0858216d4d772c1564ebe471f8932261bc03535b
Kaustubh Patil <patilkaustubh990@gmail.com>
2024-06-28 05:37:09 +0530

    refactor: Update page.js and Header.jsx components

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="px-30 py-28 mt-15 h-screen overflow-x-auto" style={{ backgroundColor: '##0f172a' }}>
+    <div className="px-30 py-28 mt-15 h-screen overflow-x-auto" style={{ backgroundColor: '#020617' }}>
 
     </div>
-   );
+  );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,13 +8,13 @@ import { OrganizationSwitcher, UserButton } from '@clerk/nextjs'
 
 const Header = () => {
   return (
-    <div className='fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center justify-center'>
+    <div className='fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm flex items-center justify-center' style={{ backgroundColor: '#020617' }}>
       <div className='hidden md:flex'>
         <Link href="/">
           <div className='hover:opacity-80 transition flex items-center gap-x-2 '>
             {/* <Image src="/logo.png" alt="logo" width={60} height={30} className='' /> */}
             {/* <h2 className={merge("text-lg text-slate-700 pb-1 ")}>KanBAN</h2> */}
-            <h2 className={merge("text-lg text-slate-700 pb-1 custom-font font-bold")}>KanBAN BoArD</h2>
+            <h2 className={merge("text-lg text-slate-300 pb-1 custom-font font-bold")}>KanBAN BoArD</h2>
           </div>
         </Link>
       </div>
