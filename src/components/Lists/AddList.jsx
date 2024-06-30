e51bba565fc7e8199b7463617d26c9ae146299f6
Kaustubh Patil <patilkaustubh990@gmail.com>
2024-06-30 23:59:30 +0530

    refactor: Update AddList component UI and button styles

--- src/components/Lists/AddList.jsx
+++ src/components/Lists/AddList.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
+import { Plus } from 'lucide-react'
 
 
 const AddList = () => {
   return (
-    <li className='shrink-0 w-[300px] h-full select-none'>
-      <Button>Add to List</Button>
+    <li className='shrink-0 h-full w-[270px]  select-none'>
+      <button className="
+            w-full rounded-lg bg-white text-black translate p-2 
+            flex items-center font-medium text-sm mt-8
+      ">
+        Add to List <Plus className='ml-2' size={36} />
+      </button>
     </li>
   )
 }
